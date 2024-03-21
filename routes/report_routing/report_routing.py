02e6b22f22b99627a9c50247dd04af304820c72a
SergeyGr07 <anonymer356@gmail.com>
2024-03-21 21:40:43 +0800

    add_tables_fill

--- routes/report_routing/report_routing.py
+++ routes/report_routing/report_routing.py
@@ -4,6 +4,8 @@ from config import add_logger
 from flask import send_file
 from docx import Document
 from io import BytesIO
+# from docx.shared import Inches
+# from openpyxl import load_workbook
 
 
 script_name = os.path.splitext(os.path.basename(__file__))[0]
@@ -20,6 +22,8 @@ def reports():
 
 @report.route('/export_docx')
 def fill_document():
+    report_file = BytesIO()
+    doc = Document("Template.docx")
 
     data = {
         'date': '21 марта 2024',
@@ -31,13 +35,34 @@ def fill_document():
         'end_time': '12:00',
         'temp': '25',
     }
-    report_file = BytesIO()
-    doc = Document("Template.docx")
+
+    data_pipe = {
+        'Труба 1': {
+            'nominal1': [356],
+            'Measurements1': list(range(300, 311)),
+            'Average1': [433],
+            'Allowance1': ["что-то"],
+            'Geometry1': ["что-то"]
+        }
+    }
 
     for paragraph in doc.paragraphs:
         for key, value in data.items():
             if '{{' + key + '}}' in paragraph.text:
                 paragraph.text = paragraph.text.replace('{{' + key + '}}', value)
+        for key, value in data_pipe.items():
+            if '{{' + key + '}}' in paragraph.text:
+                paragraph.text = paragraph.text.replace('{{' + key + '}}', value)
+
+    # Замена плейсхолдеров в таблицах
+    for table in doc.tables:
+        for row in table.rows:
+            for cell in row.cells:
+                for name, pipe_data in data_pipe.items():
+                    for key, value in pipe_data.items():
+                        placeholder = '{{' + key + '}}'
+                        if placeholder in cell.text:
+                            cell.text = cell.text.replace(placeholder, ', '.join(map(str, value)))
 
     doc.save(report_file)
     report_file.seek(0)
