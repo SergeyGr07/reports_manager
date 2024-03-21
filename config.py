cbe1983d1da2b0a852000e3baa29d25d221b17cd
SergeyGr07 <anonymer356@gmail.com>
2024-03-21 18:53:01 +0800

    fix_config

--- config.py
+++ config.py
@@ -2,9 +2,6 @@ from loguru._logger import Core as _Core
 from loguru._logger import Logger
 import os
 import sys
-from docx import Document
-# from docx.shared import Pt
-from io import BytesIO
 
 LOG_PATH = "./log"
 
@@ -30,37 +27,3 @@ def add_logger(logger_name: str, script_name: str):
 
 script_name = os.path.splitext(os.path.basename(__file__))[0]
 logger = add_logger(f'logger_{script_name}', script_name)
-
-
-def fill_document():
-    current_directory = os.path.dirname(os.path.abspath(__file__))
-    doc_path = os.path.join(current_directory, 'report.docx')
-    data = {
-        'date': '21 марта 2024',
-        'names': 'Налётов В.А, Петров П.П.',
-        'name_machine': 'Оборудование X',
-        'company': 'Компания ABC',
-        'location': 'ул. Пушкина, д.10',
-        'start_time': '10:00',
-        'end_time': '12:00',
-        'temp': '25',
-    }
-
-    # Открыть существующий документ
-    doc = Document("Template.docx")
-
-    # Заменить заполнители на соответствующие значения
-    for paragraph in doc.paragraphs:
-        for key, value in data.items():
-            if '{{' + key + '}}' in paragraph.text:
-                paragraph.text = paragraph.text.replace('{{' + key + '}}', value)
-
-    # Сохранить документ
-    doc.save(doc_path)
-
-    # Вернуть True, чтобы показать, что заполнение прошло успешно
-    return True
-
-
-
-print(fill_document())
--- routes/bot_routing/bot_routing.py
+++ routes/bot_routing/bot_routing.py
@@ -1,25 +1,25 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from telegram import Bot, Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 import requests
 
-# Инициализируем Flask приложение
 bot = Blueprint("bot", __name__)
 
-# Задаем токен вашего телеграм-бота
-TELEGRAM_BOT_TOKEN = "YTELEGRAM_BOT_TOKEN"
+TELEGRAM_BOT_TOKEN = "TELEGRAM_BOT_TOKEN"
 
 
-@app.route('/get_data', methods=['POST'])
+@bot.route('/get_data', methods=['POST'])
 def get_data():
     data = request.json
 
     return 'Data received successfully'
 
+
 def send_data_to_flask(update: Update, context: CallbackContext):
     user_input = update.message.text
     response = requests.post('http://127.0.0.1:5000/get_data', json={'user_input': user_input})
     update.message.reply_text('Data sent to Flask successfully')
+
 
 updater = Updater(token=TELEGRAM_BOT_TOKEN, use_context=True)
 dispatcher = updater.dispatcher
